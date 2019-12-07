96f1b28f606c3f65633c5b700da2e3e986b4b341
NKaty <nd.katy@gmail.com>
2019-12-07 21:29:06 +0300

    add script for creation tables in the database

---
+++ create.py
@@ -0,0 +1,20 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine(os.getenv('DATABASE_URL'))
+db = scoped_session(sessionmaker(bind=engine))
+
+
+def main():
+    db.execute('CREATE TABLE books (isbn VARCHAR PRIMARY KEY,\
+                                    title VARCHAR NOT NULL,\
+                                    author VARCHAR NOT NULL,\
+                                    year SMALLINT NOT NULL)')
+    print('The table "books" is created.')
+    db.commit()
+
+
+if __name__ == '__main__':
+    main()
