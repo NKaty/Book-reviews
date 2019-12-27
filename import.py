671e5f101c881119df88f78b5597266360749862
NKaty <nd.katy@gmail.com>
2019-12-27 17:07:26 +0300

    fix bugs in book import

--- import.py
+++ import.py
@@ -13,11 +13,13 @@ db = scoped_session(sessionmaker(bind=engine))
 
 
 def main():
-    file = open('books.csv')
-    reader = csv.reader(file)
-    for isbn, title, author, year in reader:
-        db.execute('INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)',
-                   {'isbn': isbn, 'title': title, 'author': author, 'year': year})
+    with open('books.csv') as file:
+        reader = csv.reader(file)
+        next(reader)
+        for isbn, title, author, year in reader:
+            db.execute('INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)',
+                       {'isbn': isbn, 'title': title, 'author': author, 'year': year})
+
     print('All books are added to the database.')
     db.commit()
 
