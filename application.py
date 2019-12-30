54e1c1b77f0a25cfc6e83034681cc73cdf31f736
NKaty <nd.katy@gmail.com>
2019-12-30 20:47:13 +0300

    rework review logic

--- application.py
+++ application.py
@@ -173,42 +173,50 @@ def search():
 @login_required
 def book(isbn):
     if request.method == 'GET':
-        book_data = db.execute('SELECT isbn, title, author, year, \
-                                        reviews.rating, reviews.comment, reviews.created_on, \
-                                        users.username \
-                                        FROM books \
-                                        INNER JOIN reviews ON reviews.book_isbn = books.isbn \
-                                        INNER JOIN users ON users.id = reviews.user_id \
-                                        WHERE books.isbn = :isbn',
+        book_data = db.execute("SELECT isbn, title, author, year, \
+                                reviews.rating, reviews.comment, \
+                                TO_CHAR(reviews.created_on, 'DD/MM/YYYY HH24:MI:SS') as created_on, \
+                                users.username \
+                                FROM books \
+                                INNER JOIN reviews ON reviews.book_isbn = books.isbn \
+                                INNER JOIN users ON users.id = reviews.user_id \
+                                WHERE books.isbn = :isbn \
+                                ORDER by created_on DESC",
                                {"isbn": isbn}).fetchall()
 
         # request to Goodreads API
         if len(book_data):
-            rating_data = requests.get('https://www.goodreads.com/book/review_counts.json', params={
-                'key': os.getenv('GOODREADS_KEY'),
-                'isbns': isbn
-            }).json()['books'][0]
+            try:
+                rating_data = requests.get('https://www.goodreads.com/book/review_counts.json', params={
+                    'key': os.getenv('GOODREADS_KEY'),
+                    'isbns': isbn
+                }).json()['books'][0]
+            except Exception:
+                flash('Unfortunately we cannot get information from goodreads.com.')
+                rating_data = None
 
-        print(book_data)
-        print(rating_data)
+        else:
+            flash(f'There is no book with isbn {isbn}. Please, try again.')
+            return redirect(url_for('search-form'))
 
         return render_template('book.html', book_data=book_data, rating_data=rating_data)
 
+    # POST request
     rating = request.form.get('rating')
     comment = request.form.get('comment')
 
-    id = db.execute('INSERT INTO reviews (book_isbn, user_id, rating, comment) \
-                VALUES (:book_isbn, :user_id, :rating, :comment) \
-                ON CONFLICT ON CONSTRAINT review_user_book_unique \
-                DO NOTHING \
-                RETURNING id',
-                    {'book_isbn': isbn,
-                     'user_id': session.get('user_id'),
-                     'rating': rating,
-                     'comment': comment}).fetchone()
+    review_id = db.execute('INSERT INTO reviews (book_isbn, user_id, rating, comment) \
+                            VALUES (:book_isbn, :user_id, :rating, :comment) \
+                            ON CONFLICT ON CONSTRAINT review_user_book_unique \
+                            DO NOTHING \
+                            RETURNING id',
+                           {'book_isbn': isbn,
+                            'user_id': session.get('user_id'),
+                            'rating': rating,
+                            'comment': comment}).fetchone()
     db.commit()
 
-    if id is None:
-        flash('You already submitted a review for this book')
+    if review_id is None:
+        flash('You already submitted a review for this book.')
 
     return redirect(url_for('book', isbn=isbn))
